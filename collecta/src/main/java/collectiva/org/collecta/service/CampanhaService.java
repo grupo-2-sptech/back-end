b22d7eb7ce282f40ad83a371aec0705234d4b0c1
DevEduBiel <eduardo.gabiel@sptech.school>
2023-10-17 17:00:34 -0300

    Atualizar controllers e servicos de campanha

--- collecta/src/main/java/collectiva/org/collecta/service/CampanhaService.java
+++ collecta/src/main/java/collectiva/org/collecta/service/CampanhaService.java
@@ -2,15 +2,14 @@ package collectiva.org.collecta.service;
 
 import collectiva.org.collecta.domain.Campanha;
 import collectiva.org.collecta.dto.CampanhaDTO;
+import collectiva.org.collecta.exception.exceptions.EntidadeNaoContemElementosException;
+import collectiva.org.collecta.exception.exceptions.EntidadeNaoEncontradaException;
 import collectiva.org.collecta.mapper.CampanhaMapper;
 import collectiva.org.collecta.repository.CampanhaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -19,57 +18,38 @@ import java.util.stream.Collectors;
 public class CampanhaService {
     private final CampanhaRepository campanhaRepository;
 
-    public ResponseEntity<CampanhaDTO> salvarCampanha(CampanhaDTO campanhaDTO) {
+    public CampanhaDTO salvarCampanha(CampanhaDTO campanhaDTO) {
         Campanha campanha = CampanhaMapper.paraEntidade(campanhaDTO);
         campanhaRepository.save(campanha);
-        campanhaDTO = CampanhaMapper.paraDTO(campanha);
-        return ResponseEntity.status(HttpStatus.CREATED).body(campanhaDTO);
+        return CampanhaMapper.paraDTO(campanha);
     }
 
-    public ResponseEntity<List<CampanhaDTO>> buscarTodasCampanhas() {
+    public List<CampanhaDTO> buscarTodasCampanhas() {
         List<Campanha> campanha = campanhaRepository.findAll();
         if (campanha.isEmpty()) {
-            return ResponseEntity.noContent().build();
+            throw new EntidadeNaoContemElementosException("Campanha");
         }
-        List<CampanhaDTO> campanhaDTOs = campanha.stream().map(CampanhaMapper::paraDTO).collect(Collectors.toList());
-        return ResponseEntity.ok().body(campanhaDTOs);
+        return campanha.stream().map(CampanhaMapper::paraDTO).collect(Collectors.toList());
     }
 
-    public ResponseEntity<CampanhaDTO> buscarCampanhaPorId(UUID id) {
-        Optional<Campanha> campanha = campanhaRepository.findById(id);
-        if (campanha.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        CampanhaDTO campanhaDTO = CampanhaMapper.paraDTO(campanha.get());
-        return ResponseEntity.ok().body(campanhaDTO);
+    public CampanhaDTO buscarCampanhaPorId(UUID id) {
+        return CampanhaMapper.paraDTO(campanhaRepository.findById(id).orElseThrow(
+                () -> new EntidadeNaoEncontradaException("Campanha")));
     }
 
-    public ResponseEntity<CampanhaDTO> atualizarCampanha(UUID id, CampanhaDTO campanhaDTO) {
-        Optional<Campanha> campanhaAntiga = campanhaRepository.findById(id);
-        if (campanhaAntiga.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        Campanha campanhaExistente = campanhaAntiga.get();
-        Campanha campanhaAtualizada = Campanha.builder()
-                .id(campanhaExistente.getId())
-                .nome(campanhaDTO.getNome())
-                .descricao(campanhaDTO.getDescricao())
-                .dataInicio(campanhaDTO.getDataInicio())
-                .dataFim(campanhaDTO.getDataFim())
-                .status(campanhaDTO.getStatus())
-                .build();
-
-        campanhaRepository.save(campanhaAtualizada);
-        campanhaDTO = CampanhaMapper.paraDTO(campanhaAtualizada);
-        return ResponseEntity.ok().body(campanhaDTO);
+    public CampanhaDTO atualizarCampanha(UUID id, CampanhaDTO campanhaDTO) {
+        buscarCampanhaPorId(id);
+        Campanha campanhaNova = CampanhaMapper.paraEntidade(campanhaDTO);
+        campanhaNova.setId(id);
+        campanhaRepository.save(campanhaNova);
+        return CampanhaMapper.paraDTO(campanhaNova);
     }
 
-    public ResponseEntity<Void> deletarCampanha(UUID id) {
+    public void deletarCampanha(UUID id) {
         if (!campanhaRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
+            throw new EntidadeNaoEncontradaException("Campanha");
         }
         campanhaRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 }
 
