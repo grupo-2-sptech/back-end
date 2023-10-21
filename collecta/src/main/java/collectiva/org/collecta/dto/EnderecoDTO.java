59bd24a6d419ef15e141bd193765214789bac688
Gabriel Cavalcanti <gabriel.gsilva@sptech.school>
2023-10-21 13:13:22 -0300

    Adicionando CepController e ajustando annotations

--- collecta/src/main/java/collectiva/org/collecta/dto/EnderecoDTO.java
+++ collecta/src/main/java/collectiva/org/collecta/dto/EnderecoDTO.java
@@ -1,27 +1,34 @@
 package collectiva.org.collecta.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class EnderecoDTO {
+    @JsonIgnore
     private UUID id;
 
     @NotBlank(message = "O logradouro esta vazio")
     @Size(min = 4, message = "O logradouro deve ter no mínimo 4 caracteres")
     private String logradouro;
 
+    @JsonProperty("localidade")
     @NotBlank(message = "A cidade esta vazia")
     @Size(min = 4, message = "A cidade deve ter no mínimo 4 caracteres")
     private String cidade;
 
+    @JsonProperty("uf")
     @NotBlank(message = "O estado esta vazio")
     @Size(min = 2, max = 2, message = "O logradouro deve ter 2 caracteres")
     private String estado;
@@ -30,6 +37,7 @@ public class EnderecoDTO {
     @Size(min = 8, max = 8, message = "O cep deve ter 8 caracteres")
     private String cep;
 
+    @JsonIgnore
     @NotNull
     @Min(value = 1, message = "O número de parcelas deve ser pelo menos 1")
     @Max(value = 999999, message = "O número de parcelas não pode exceder 999999")
