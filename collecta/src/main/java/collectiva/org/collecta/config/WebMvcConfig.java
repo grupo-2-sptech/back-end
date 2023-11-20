43ac2173bed253a8d70fcc994139af0ee9f36aa7
DevEduBiel <eduardo.gabiel@sptech.school>
2023-11-20 09:36:05 -0300

    Previnir conflito de cors

--- collecta/src/main/java/collectiva/org/collecta/config/WebMvcConfig.java
+++ collecta/src/main/java/collectiva/org/collecta/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import collectiva.org.collecta.domain.campanha.interceptor.CampanhaViewIntercept
 import collectiva.org.collecta.domain.campanha.service.CampanhaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +19,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         CampanhaViewInterceptor campanhaViewInterceptor = new CampanhaViewInterceptor(campanhaService);
         registry.addInterceptor(campanhaViewInterceptor);
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+        }
     }
 }
