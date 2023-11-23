418e2ee8f8253bbbed6f95d3d7ab356ecda8e9bc
DevEduBiel <eduardo.gabiel@sptech.school>
2023-11-23 10:24:32 -0300

    Aplicar dependencia de outra entidade em Campanha + UpdateDTO

---
+++ collecta/src/main/java/collectiva/org/collecta/domain/campanha/dto/UpdateCampanhaDTO.java
@@ -0,0 +1,45 @@
+package collectiva.org.collecta.domain.campanha.dto;
+
+import collectiva.org.collecta.enums.CategoriaCampanha;
+import collectiva.org.collecta.enums.StatusCampanha;
+import collectiva.org.collecta.enums.TipoCampanha;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class CreateCampanhaDTO {
+    @NotBlank(message = "O nome está vazio")
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres")
+    private String nome;
+
+    @NotBlank(message = "A descrição está vazia")
+    @Size(min = 3, message = "A descrição deve ter no mínimo 3 caracteres")
+    private String descricao;
+
+    @NotNull(message = "A data de inicio está vazia")
+    @FutureOrPresent(message = "A data de início deve estar no futuro ou presente")
+    private LocalDateTime dataInicio;
+
+    @NotNull(message = "A data do fim está vazia")
+    @Future(message = "A data de fim deve estar no futuro")
+    private LocalDateTime dataFim;
+
+    @NotNull(message = "A categoria da campanha está vazia")
+    @Enumerated(EnumType.STRING)
+    private CategoriaCampanha categoriaCampanha;
+
+    @NotNull(message = "O tipo da campanha está vazio")
+    @Enumerated(EnumType.STRING)
+    private TipoCampanha tipoCampanha;
+
+    @NotNull(message = "O id da organização está vazio")
+    private UUID idOrganizacao;
+
+}
